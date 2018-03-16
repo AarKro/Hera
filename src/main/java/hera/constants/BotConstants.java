f2890372ba1e1df1e2932f3a8b13ecb3f75c7c66
Chromer <kromer.aaron@gmail.com>
2018-03-16 20:57:48 +0100

    Added logging with log4j and slf4j. Currently only implemented in Main.class

--- src/main/java/hera/constants/BotConstants.java
+++ src/main/java/hera/constants/BotConstants.java
@@ -2,7 +2,7 @@ package hera.constants;
 
 public class BotConstants {
 	//property location constants
-	public static final String PROPERTIES_BASE_LOCATION = "properties/";
+	public static final String PROPERTIES_BASE_LOCATION = "./properties/";
 	public static final String BINDING_PROPERTY_LOCATION = "binding.properties";
 	public static final String SETTINGS_PROPERTY_LOCATION = "settings.properties";
 	public static final String CLIENT_PROPERTY_LOCATION = "client.properties";
--- src/main/java/hera/main/Main.java
+++ src/main/java/hera/main/Main.java
@@ -1,5 +1,8 @@
 package hera.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 
@@ -13,28 +16,55 @@ import sx.blah.discord.api.events.EventDispatcher;
 public class Main {
 	
 	public static void main(String[] args) {
+		Logger LOG = LoggerFactory.getLogger(Main.class);
+		LOG.debug("Start of: main");
+		LOG.info("Starting Hera...");
 		
+		LOG.info("Starting project initialization");
 		ProjectInitalizer projectInitalizer = new ProjectInitalizer();
+		
+		LOG.info("Initialize properties");
 		projectInitalizer.initalizeProperties();
+		LOG.info("Properties initialized");
+		
+		LOG.info("Create and login client instance");
 		ClientManager cm = ClientManager.getInstance();
 		
 		try {
+			LOG.info("Waiting for login to finish");
 			Thread.sleep(5000);
 		} catch (InterruptedException e1) {
-			e1.printStackTrace();
+			LOG.error("Login failed");
+			LOG.error(e1.getMessage() + " : " + e1.getCause());
 		}
+		LOG.info("Login successful");
 		
+		LOG.info("Initialize channel bindings");
 		projectInitalizer.initalizeBindings(cm.getiDiscordClient().getGuilds().get(0));
+		LOG.info("Channel bindings initialized");
+
+		LOG.info("Initialize YouTube properties");
 		projectInitalizer.initializeYoutubeProperties();
+		LOG.info("YouTube properties initialized");
 		
+		LOG.info("Register audio sources");
 		AudioPlayerManager apm = SingletonInstancer.getAPMInstance();
 		AudioSourceManagers.registerRemoteSources(apm);
 		AudioSourceManagers.registerLocalSource(apm);
+		LOG.info("Audio sources registered");
 		
+		LOG.info("Register event listeners");
 		EventDispatcher ed = cm.getiDiscordClient().getDispatcher();
 		ed.registerListener(new AutoAssignRole());
 		ed.registerListener(new HandleMessageRecievedEvent());
+		LOG.info("Event listeners registered");
 		
+		//LOG.info("Setting discord presence");
 		//cm.getiDiscordClient().changePresence(StatusType.ONLINE, ActivityType.WATCHING, "over you ಠ_ಠ");
+		//LOG.info("Discord presence set");
+		
+		LOG.info("Project initialization finished");
+		LOG.info("Hera is now ready to use");
+		LOG.debug("End of: main");
 	}
 }
