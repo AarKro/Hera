d2efe7d535a2df8a096e8ec1fc777fe5d59b5f43
Aaron Kromer <kromer.aaron@gmail.com>
2019-11-13 22:41:26 +0000

    some restructuring

--- environment/scripts/setup_db.sh
+++ environment/scripts/setup_db.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+start=$PWD
+script=`dirname "$BASH_SOURCE"`
+cd $script
+cd ../sql
+
 commands=$(mktemp)
 trap "rm -f $commands" 0 2 3 15
-
-dir=`dirname "$BASH_SOURCE"`
 
 echo "DROP DATABASE IF EXISTS $HERA_DB_NAME;" >> $commands
 echo "CREATE DATABASE $HERA_DB_NAME;" >> $commands
 echo "USE $HERA_DB_NAME;" >> $commands
-cat $dir/tables/* $dir/constraints.sql >> $commands
+cat tables/* constraints.sql >> $commands
 
 mysql -u $HERA_DB_USER -p$HERA_DB_PWD < $commands
+
+cd $start
