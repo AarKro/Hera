de718fa748429d0ca51ab87952b6d20eaa43a55d
Aaron Kromer <kromer.aaron@gmail.com>
2019-11-14 21:19:48 +0000

    Updated environment variable script so it automatically untracks itself

--- environment/scripts/env_var.sh
+++ environment/scripts/env_var.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Untrack script in git so local changes to this script won't be staged by git
+start=$PWD
+script=`dirname "$BASH_SOURCE"`
+
+cd $script
+git update-index --assume-unchanged env_var.sh
+cd $start
+
 # Hera environment variables
-export HERA_DB_URL='#URL'
-export HERA_DB_USER='#USER'
-export HERA_DB_PWD='#PASSWORD'
-export HERA_DB_NAME='#NAME'
+export HERA_DB_URL='#DB_URL'
+export HERA_DB_USER='#DB_USER'
+export HERA_DB_PWD='#DB_PASSWORD'
+export HERA_DB_NAME='DB_#NAME'
