3a2da3fe09f4cc07ab81cbbfddaf035ba87846fe
Aaron Kromer <kromer.aaron@gmail.com>
2019-12-19 20:48:24 +0000

    corrected some accidental sql mistakes

--- environment/sql/data.sql
+++ environment/sql/data.sql
@@ -23,9 +23,9 @@ INSERT INTO `user` (`snowflake`) VALUES
 INSERT INTO `command` (`id`, `name`, `description`, `paramCount`, `infiniteParam`, `admin`) VALUES
 (0, 'UPTIME', 'Checks how long Hera has been up and running', 0, 0, 1),
 (1, 'VERSION', 'Displays current version number', 0, 0, 0),
-(2, 'HELP', 'Shows this Help page.', 0, 0, 0);
-(3, 'DELETEMESSAGES', 'Deletes the newest channel messages.', 1, 0, 1);
-(4, 'ALIAS', 'Creates an alias for a command that works in only the current guild.', 2, 0, 1);
+(2, 'HELP', 'Shows this Help page.', 0, 0, 0),
+(3, 'DELETEMESSAGES', 'Deletes the newest channel messages.', 1, 0, 1),
+(4, 'ALIAS', 'Creates an alias for a command that works in only the current guild.', 2, 0, 1),
 (5, 'PREFIX', 'Sets command prefix.', 1, 0, 1);
 
 INSERT INTO `token` (`id`, `token`, `name`, `description`) VALUES
@@ -34,7 +34,7 @@ INSERT INTO `token` (`id`, `token`, `name`, `description`) VALUES
 INSERT INTO `localisation` (`language`, `name`, `value`) VALUES
 ('en', 'COMMAND_UPTIME', 'I am up and running for the last %s'),
 ('en', 'COMMAND_VERSION', 'I am running on version %s'),
-('en', 'COMMAND_HELP', 'Commands');
+('en', 'COMMAND_HELP', 'Commands'),
 ('en', 'COMMAND_PREFIX', 'Prefix set to: %s');
 
 INSERT INTO `global_settings` (`id`, `name`, `value`) VALUES
