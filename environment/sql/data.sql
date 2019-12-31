5a0751054ba7a11f22996d5f8279b9c643249b53
Aaron Kromer <kromer.aaron@gmail.com>
2019-12-31 16:19:43 +0100

    Increment version

--- environment/sql/data.sql
+++ environment/sql/data.sql
@@ -84,7 +84,7 @@ INSERT INTO `localisation` (`language`, `name`, `value`) VALUES
 ('en', 'COMMAND_JUMPTO_ERROR', 'Can not jump to queue index %s');
 
 INSERT INTO `global_setting` (`id`, `name`, `value`) VALUES
-(0, 'VERSION', '2.0.0-alpha.0');
+(0, 'VERSION', '2.0.0-alpha.1');
 
 INSERT INTO `alias` (`id`, `commandFK`, `alias`) VALUES
 (1, 8, 'p'),
