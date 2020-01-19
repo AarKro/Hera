59126226af5d4a57fac65769d17647f23c7d1fa4
Pacmens <leberhard@gmx.ch>
2020-01-19 18:54:11 +0100

    changed imports in flip and teams

--- core/src/main/java/hera/core/commands/Flip.java
+++ core/src/main/java/hera/core/commands/Flip.java
@@ -5,16 +5,12 @@ import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.MessageChannel;
 import hera.core.HeraUtil;
-import hera.database.entities.mapped.GlobalSetting;
-import hera.database.entities.mapped.Localisation;
-import hera.database.types.GlobalSettingKey;
+import hera.database.entities.Localisation;
 import hera.database.types.LocalisationKey;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Random;
-
-import static hera.store.DataStore.STORE;
 
 public class Flip {
 	public static Mono<Void> execute(MessageCreateEvent event, Guild guild, Member member, MessageChannel channel, List<String> params) {
