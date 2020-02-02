023512ca46a17247465090eeec68bd07673c8e2e
Aaron Kromer <kromer.aaron@gmail.com>
2020-02-02 21:15:24 +0000

    Fixed hibernate stuff (#193)

--- data/src/main/java/hera/store/unit/ModuleSettingsAccessUnit.java
+++ data/src/main/java/hera/store/unit/ModuleSettingsAccessUnit.java
@@ -1,5 +1,6 @@
 package hera.store.unit;
 
+import hera.database.entities.Command;
 import hera.database.entities.ModuleSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,11 +20,11 @@ public class ModuleSettingsAccessUnit extends StorageAccessUnit<ModuleSettings>{
 		return get(Collections.singletonMap("guild", guild));
 	}
 
-	public List<ModuleSettings> forCommand(Long command) {
+	public List<ModuleSettings> forCommand(Command command) {
 		return get(Collections.singletonMap("command", command));
 	}
 
-	public List<ModuleSettings> forModule(Long guild, Long command) {
+	public List<ModuleSettings> forModule(Long guild, Command command) {
 		return get(new LinkedHashMap<String, Object>() {{
 			put("guild", guild);
 			put("command", command);
