1ca31f6f6d536fd5c541fc908a5e99bd1e9b3e5d
Aaron Kromer <kromer.aaron@gmail.com>
2020-07-16 23:19:01 +0100

    Added ConfigFlagType entries. Added Metric logging flag

--- data/src/main/java/hera/database/types/ConfigFlagType.java
+++ data/src/main/java/hera/database/types/ConfigFlagType.java
@@ -1,4 +1,8 @@
 package hera.database.types;
 
 public enum ConfigFlagType {
+	METRIC_LOGGING,
+	JOIN_ON_PLAY,
+	CLEAR_QUEUE_ON_LEAVE,
+	ANNOUNCE_NEXT_SONG;
 }
--- metrics/src/main/java/hera/metrics/MetricsLogger.java
+++ metrics/src/main/java/hera/metrics/MetricsLogger.java
@@ -1,13 +1,16 @@
 package hera.metrics;
 
 import hera.database.entities.Command;
+import hera.database.entities.ConfigFlag;
 import hera.database.entities.Metric;
+import hera.database.types.ConfigFlagType;
 import hera.database.types.MetricKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static hera.store.DataStore.STORE;
 
@@ -19,36 +22,61 @@ public class MetricsLogger {
 	private MetricsLogger() {
 	}
 
+	private boolean isLoggingEnabled(Long guild) {
+		List<ConfigFlag> flags = STORE.configFlags().forGuildAndType(guild, ConfigFlagType.METRIC_LOGGING);
+
+		if (!flags.isEmpty()) {
+			return flags.get(0).getValue();
+		}
+
+		return true;
+	}
 	public void log(MetricKey key, Command command, Long user, Long guild, Long value, String details) {
-		STORE.metrics().add(new Metric(key, Timestamp.valueOf(LocalDateTime.now()), command, guild, user, value, details));
+		if (isLoggingEnabled(guild)) {
+			STORE.metrics().add(new Metric(key, Timestamp.valueOf(LocalDateTime.now()), command, guild, user, value, details));
+		}
 	}
 
 	public void logMessage(Long user, Long guild) {
-		STORE.metrics().add(new Metric(MetricKey.MESSAGE, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		if (isLoggingEnabled(guild)) {
+			STORE.metrics().add(new Metric(MetricKey.MESSAGE, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		}
 	}
 
 	public void logCommand(Command command, Long user, Long guild) {
-		STORE.metrics().add(new Metric(MetricKey.COMMAND, Timestamp.valueOf(LocalDateTime.now()), command, guild, user, null, null));
+		if (isLoggingEnabled(guild)) {
+			STORE.metrics().add(new Metric(MetricKey.COMMAND, Timestamp.valueOf(LocalDateTime.now()), command, guild, user, null, null));
+		}
 	}
 
 	public void logVcJoin(Long user, Long guild) {
-		STORE.metrics().add(new Metric(MetricKey.VC_JOIN, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		if (isLoggingEnabled(guild)) {
+			STORE.metrics().add(new Metric(MetricKey.VC_JOIN, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		}
 	}
 
 	public void logVcLeave(Long user, Long guild) {
-		STORE.metrics().add(new Metric(MetricKey.VC_LEAVE, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		if (isLoggingEnabled(guild)) {
+			STORE.metrics().add(new Metric(MetricKey.VC_LEAVE, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		}
 	}
 
 	// user will just be Heras Id at this point
 	public void logHeraGuildJoin(Long user, Long guild) {
-		STORE.metrics().add(new Metric(MetricKey.HERA_GUILD_JOIN, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		if (isLoggingEnabled(guild)) {
+			STORE.metrics().add(new Metric(MetricKey.HERA_GUILD_JOIN, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		}
 	}
 
 	public void logUserGuildJoin(Long user, Long guild) {
-		STORE.metrics().add(new Metric(MetricKey.USER_GUILD_JOIN, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		if (isLoggingEnabled(guild)) {
+			STORE.metrics().add(new Metric(MetricKey.USER_GUILD_JOIN, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		}
 	}
 
 	public void logUserGuildLeave(Long user, Long guild) {
-		STORE.metrics().add(new Metric(MetricKey.USER_GUILD_LEAVE, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		if (isLoggingEnabled(guild)) {
+			STORE.metrics().add(new Metric(MetricKey.USER_GUILD_LEAVE, Timestamp.valueOf(LocalDateTime.now()), null, guild, user, null, null));
+		}
 	}
 }
