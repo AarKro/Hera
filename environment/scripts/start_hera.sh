32ab0780c68e2ebec57b7c0425b753e184a0ce43
Aaron Kromer <kromer.aaron@gmail.com>
2020-07-20 15:49:42 +0100

    Create start_hera.sh

---
+++ environment/scripts/start_hera.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#######################################################################################
+# THIS SCRIPT IS AUTOMATICALLY TRIGGERED BY AWS CODE DEPLOY                           #
+# AFTER THE HERA JAR IS BUILT AND UPLOADED TO EC2 AND RUNS ON                         #
+# THE EC2 INSTANCE ITSELF                                                             #
+#                                                                                     #
+# THIS SCRIPT WILL NOT WORK LOCALLY AND WAS NEVER INTENDED TO DO SO                   #
+#######################################################################################
+
+cd "$(dirname "$0")"
+cd ..
+
+# Terminate running jar processes as in terminating Hera
+pkill -f 'java -jar'
+
+# Close all running screen sessions
+pkill screen
+
+echo "$(date): starting hera"
+
+screen -dmS hera bash -c 'java -jar bots/hera-prod-bundle-jar-with-dependencies.jar'
