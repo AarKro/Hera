3e3fcaf1f145a003083469258c8ca7617749deef
Pacmens <leberhard@gmx.ch>
2021-07-01 12:19:47 +0200

    fixed indention in volume file

--- core/src/main/java/hera/core/commands/Volume.java
+++ core/src/main/java/hera/core/commands/Volume.java
@@ -19,37 +19,37 @@ import java.util.List;
 import static hera.store.DataStore.STORE;
 
 public class Volume {
-    public static Mono<Void> execute(MessageCreateEvent event, Guild guild, Member member, MessageChannel channel, List<String> params) {
-        Integer volume = Integer.parseInt(params.get(0));
+	public static Mono<Void> execute(MessageCreateEvent event, Guild guild, Member member, MessageChannel channel, List<String> params) {
+		Integer volume = Integer.parseInt(params.get(0));
 
-        if (volume < 0 || volume > 100) {
-            Localisation local = HeraUtil.getLocalisation(LocalisationKey.COMMAND_VOLUME_ERROR, guild);
-            String message = String.format(local.getValue(), volume);
+		if (volume < 0 || volume > 100) {
+			Localisation local = HeraUtil.getLocalisation(LocalisationKey.COMMAND_VOLUME_ERROR, guild);
+			String message = String.format(local.getValue(), volume);
 
-            return MessageHandler.send(channel, MessageSpec.getErrorSpec(messageSpec -> messageSpec.setDescription(message))).then();
-        }
+			return MessageHandler.send(channel, MessageSpec.getErrorSpec(messageSpec -> messageSpec.setDescription(message))).then();
+		}
 
-        List<GuildSetting> gsList = STORE.guildSettings().forGuildAndKey(guild.getId().asLong(), GuildSettingKey.VOLUME);
+		List<GuildSetting> gsList = STORE.guildSettings().forGuildAndKey(guild.getId().asLong(), GuildSettingKey.VOLUME);
 
-        if (gsList.isEmpty()) {
-            STORE.guildSettings().add(new GuildSetting(guild.getId().asLong(), GuildSettingKey.VOLUME, volume.toString()));
-        } else {
-            GuildSetting gs = gsList.get(0);
-            gs.setValue(volume.toString());
-            STORE.guildSettings().update(gs);
-        }
+		if (gsList.isEmpty()) {
+			STORE.guildSettings().add(new GuildSetting(guild.getId().asLong(), GuildSettingKey.VOLUME, volume.toString()));
+		} else {
+			GuildSetting gs = gsList.get(0);
+			gs.setValue(volume.toString());
+			STORE.guildSettings().update(gs);
+		}
 
-        HeraAudioManager.getPlayer(guild).setVolume(volume);
+		HeraAudioManager.getPlayer(guild).setVolume(volume);
 
-        Localisation local;
-        if (volume == 0) {
-            local = HeraUtil.getLocalisation(LocalisationKey.COMMAND_VOLUME_MUTE, guild);
-        } else {
-            local = HeraUtil.getLocalisation(LocalisationKey.COMMAND_VOLUME, guild);
-        }
+		Localisation local;
+		if (volume == 0) {
+			local = HeraUtil.getLocalisation(LocalisationKey.COMMAND_VOLUME_MUTE, guild);
+		} else {
+			local = HeraUtil.getLocalisation(LocalisationKey.COMMAND_VOLUME, guild);
+		}
 
-        String message = String.format(local.getValue(), volume);
+		String message = String.format(local.getValue(), volume);
 
-        return MessageHandler.send(channel, MessageSpec.getDefaultSpec(messageSpec -> messageSpec.setDescription(message))).then();
-    }
+		return MessageHandler.send(channel, MessageSpec.getDefaultSpec(messageSpec -> messageSpec.setDescription(message))).then();
+	}
 }
